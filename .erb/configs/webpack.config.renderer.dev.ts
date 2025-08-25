533ddcdcf319c901d4a025227bd8bfe79d183fe4
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-08-24 21:17:41 -0400

    spelling: overridden
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- .erb/configs/webpack.config.renderer.dev.ts
+++ .erb/configs/webpack.config.renderer.dev.ts
@@ -118,7 +118,7 @@ const configuration: webpack.Configuration = {
      * NODE_ENV should be production so that modules do not perform certain
      * development checks
      *
-     * By default, use 'development' as NODE_ENV. This can be overriden with
+     * By default, use 'development' as NODE_ENV. This can be overridden with
      * 'staging', for example, by changing the ENV variables in the npm scripts
      */
     new webpack.EnvironmentPlugin({
